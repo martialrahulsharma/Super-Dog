db65c7673425b89101d7dbbedd1d319a1663a2e4
Martial Rahul Sharma <76786601+martialrahulsharma@users.noreply.github.com>
2022-09-10 23:10:42 +0530

    added main file

---
+++ assets/scripts/UI.js
@@ -0,0 +1,42 @@
+export class UI {
+    constructor(game) {
+        this.game = game;
+        this.fontSize = 30;
+        this.fontFamily = 'Helvetica';
+        this.liveImage = document.getElementById('lives');
+    }
+    draw(context) {
+        context.save();
+        context.shadowOffsetX = 2;
+        context.shadowOffsetY = 2;
+        context.shadowColor = 'white';
+        context.shadowBlur = 0;
+        context.font = this.fontSize + 'px ' + this.fontFamily;
+        context.textAlign = 'left';
+        context.fillStyle = this.game.fontColor;
+        context.fillText('Score: ' + this.game.score, 20, 50);
+        //timer
+        context.font = this.fontSize * 0.8 + 'px ' + this.fontFamily;
+        context.fillText('Time: ' + (this.game.time * 0.001).toFixed(1), 20, 80);
+        //lives
+        for (let i=0; i < this.game.lives; i++) {
+            context.drawImage(this.liveImage, 20 * i + 20, 95, 25, 25);
+
+        }
+        //game over message
+        if (this.game.gameOver) {
+            context.textAlign = 'center';
+            context.font = this.fontSize * 0.2 + 'px ' + this.fontFamily;
+            if (this.game.score > this.game.winningScore){
+                context.fillText('Boo-yah: ', this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily;
+                context.fillText('What are creature of the night afraid of? YOU!!!', this.game.width * 0.5, this.game.height * 0.5 + 20);
+            } else {
+                context.fillText('Love at first bite?', this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily;
+                context.fillText('Nope! Better Luck Next Time!', this.game.width * 0.5, this.game.height * 0.5 - 20);
+            }
+            context.restore();
+        }
+    }
+}
---
+++ assets/scripts/main.js
@@ -0,0 +1,107 @@
+import { Player } from './player.js';
+import { InputHandler } from './input.js';
+import { Background } from './background.js';
+import { FlyingEnemy, GroundEnemy, ClimbingEnemy } from './enemies.js';
+import { UI } from './UI.js';
+
+
+const customTime = parseInt(prompt('Enter Game Time in Minutes: ',5));
+window.addEventListener('load', function() {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 900;
+    canvas.height = 500;
+
+    class Game {
+        constructor(width, height) {
+            this.width = width;
+            this.height = height;
+            this.groundMargin = 80;
+            this.speed = 0;
+            this.maxSpeed = 3;
+            this.background = new Background(this);
+            this.player = new Player(this);
+            this.input = new InputHandler(this);
+            this.Ui = new UI(this);
+            this.enemies = [];
+            this.particles = [];
+            this.collisions = [];
+            this.maxParticles = 50;
+            this.enemyTimer = 0;
+            this.enemyInterval = 1000;
+            this.debug = false;
+            this.score = 0;
+            this.fontColor = 'black';
+            this.winningScore = 40;
+            this.time = 0;
+            this.maxTime = customTime>0?(customTime*1000*60):30000;
+            // this.maxTime = 30000 * 60;
+            this.gameOver = false;
+            this.lives = 5;
+            this.player.currentState = this.player.states[0];
+            this.player.currentState.enter();
+        }
+        update(deltaTime) {
+            this.time += deltaTime;
+            if (this.time > this.maxTime) this.gameOver = true;
+            this.background.update();
+            this.player.update(this.input.keys, deltaTime);
+            //handle enemy
+            if (this.enemyTimer > this.enemyInterval) {
+                this.addEnenmy();
+                this.enemyTimer = 0;
+            } else {
+                this.enemyTimer += deltaTime;
+            }
+            this.enemies.forEach(enemy => {
+                enemy.update(deltaTime);
+                if (enemy.markForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1);
+            });
+            // handle particles
+            this.particles.forEach((particle, index) => {
+                particle.update();
+                if (particle.markForDeletion) this.particles.splice(index, 1);
+            });
+            if (this.particles.length > this.maxParticles) {
+                this.particles.length = this.maxParticles;
+            }
+            //handle collision sprites
+            this.collisions.forEach((collision, index) => {
+                collision.update(deltaTime);
+                if (collision.markForDeletion) this.collisions.splice(index, 1);
+            });
+        }
+        draw(context) {
+            this.background.draw(context);
+            this.player.draw(context);
+            this.enemies.forEach(enemy => {
+                enemy.draw(context);  
+            });
+            this.particles.forEach(particle => {
+                particle.draw(context);  
+            });
+            this.collisions.forEach(collision => {
+                collision.draw(context);  
+            });
+            this.Ui.draw(context);
+        }
+        addEnenmy(){
+            if (this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
+            else if(this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
+            this.enemies.push(new FlyingEnemy(this));
+        }
+    }
+
+    const game = new Game(canvas.width, canvas.height);
+    let lastTime = 0;
+    function animate(timeStamp) {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.update(deltaTime);
+        game.draw(ctx);
+        if (!game.gameOver) requestAnimationFrame(animate);
+        // requestAnimationFrame(animate);
+    }
+    animate(0);
+});
